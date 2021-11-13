6346b020625e4d26f7bf091bb2719709431fb028
Nikhil Kakkar <44113004+NikhilKakkar22@users.noreply.github.com>
2021-11-13 19:19:00 +0530

    NewLine Test case Added

--- src/CalculatorTestCode/CalculatorTest.java
+++ src/CalculatorTestCode/CalculatorTest.java
@@ -18,10 +18,15 @@ public class CalculatorTest {
 	@Test
 	public void testTwoNumbers(){
 		assertEquals(3, Calculator.add("1,2"));
-	}  
-
+	}
+	
 	@Test
     public void testThreeNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
+    }
+
+	@Test
+    public void testNewLine(){
+    	assertEquals(6, Calculator.add("1\n2,3"));
     }
 }
