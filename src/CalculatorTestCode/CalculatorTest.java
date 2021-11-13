3e99fd83b5f8997935baf2a5db8d485505dcf2a1
Nikhil Kakkar <44113004+NikhilKakkar22@users.noreply.github.com>
2021-11-13 19:08:47 +0530

    calculator now working with one number

--- src/CalculatorTestCode/CalculatorTest.java
+++ src/CalculatorTestCode/CalculatorTest.java
@@ -1,18 +1,21 @@
 package CalculatorTestCode;
-
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-
 import CalculatorMainCode.Calculator;
 
 public class CalculatorTest {
 
 	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+      org.junit.runner.JUnitCore.main("CalculatorTestCode");
     }
 
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
-}  
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
+}
