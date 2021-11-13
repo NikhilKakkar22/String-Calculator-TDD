39fecf1275735c127775a2482f3ff3a8814d62e3
Nikhil Kakkar <44113004+NikhilKakkar22@users.noreply.github.com>
2021-11-13 19:13:57 +0530

    Addition with two numbers test case added

--- src/CalculatorTestCode/CalculatorTest.java
+++ src/CalculatorTestCode/CalculatorTest.java
@@ -2,20 +2,21 @@ package CalculatorTestCode;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import CalculatorMainCode.Calculator;
-
 public class CalculatorTest {
-
 	public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("CalculatorTestCode");
     }
-
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
-
 	@Test
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
+	}
+
+	@Test
+	public void testTwoNumbers(){
+		assertEquals(3, Calculator.add("1,2"));
 	}
 }
